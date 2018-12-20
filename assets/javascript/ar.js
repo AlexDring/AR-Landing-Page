f717c73c64a6b5e2287b5718178455d1cc00981c
Alex Dring <38809859+Dringr@users.noreply.github.com>
2018-12-20 10:04:56 +0000

    #

--- assets/javascript/ar.js
+++ assets/javascript/ar.js
@@ -83,3 +83,52 @@ function fourAction() {
   allHidden();
   document.getElementById("action-4").classList.toggle("show-slideshow");
 }
+
+
+// // Get the modal
+// var modal = document.getElementsByClassName('myModal');
+
+// // Get the button that opens the modal
+// var btn = document.getElementsByClassName("myBtn");
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // // When the user clicks on the button, open the modal 
+// // btn.onclick = function() {
+// //   modal.style.display = "block";
+// // }
+
+// // // When the user clicks on <span> (x), close the modal
+// // span.onclick = function() {
+// //   modal.style.display = "none";
+// // }
+
+
+// for(let i=0;i<btn.length;i++){
+//   btn[i].onclick = function() {
+//     modal[i].style.display = "block";
+//   }
+// }
+
+// for(let i=0;i<span.length;i++){
+//   span[i].onclick = function() {
+//      modal[i].style.display = "none";
+//   }
+// }
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     for(let i=0;i<btn.length;i++){
+//         modal[i].style.display = "none";
+      
+//     }
+//   }
+// }
+
+// // // When the user clicks anywhere outside of the modal, close it
+// // window.onclick = function(event) {
+// //   if (event.target == modal) {
+// //     modal.style.display = "none";
+// //   }
+// // }
